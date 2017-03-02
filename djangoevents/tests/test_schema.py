eb55176fb5833709a4c1d356bca806e068ea4411
Piotr Duda <pduda@applause.com>
2017-03-02 15:22:03 +0100

    Utils unit tests

--- djangoevents/tests/test_schema.py
+++ djangoevents/tests/test_schema.py
@@ -1,35 +1,71 @@
 import avro.schema
+import djangoevents.schema as schema
 import json
 import pytest
+from djangoevents.domain import BaseAggregate
+from djangoevents.domain import DomainEvent
+from .test_domain import SampleEntity
 from io import StringIO
-
-from ..schema import decode_cls_name, parse_event_schema, validate_event, event_to_schema_path
 from django.test import override_settings
+from unittest import mock
 
 
-SAMPLE_EVENT_SCHEMA = json.dumps({
+class Project(BaseAggregate):
+    class Created(BaseAggregate.Created):
+        def mutate_event(self, event, klass):
+            return klass(
+                entity_id=event.entity_id,
+                entity_version=event.entity_version,
+                domain_event_id=event.domain_event_id,
+                name=event.name,
+            )
+
+    class Closed(DomainEvent):
+        # no mutate_event
+        pass
+
+    def __init__(self, schema_version, project_id, name, **kwargs):
+        super().__init__(**kwargs)
+        self.schema_version = schema_version
+        self.project_id = project_id
+        self.name = name
+
+
+PROJECT_CREATED_SCHEMA = json.dumps({
     "name": "project_created",
     "type": "record",
     "doc": "Event posted when a project has been created",
     "namespace": "services.project_svc",
     "fields": [
         {
-            "name": "schema_version",
-            "type": "int",
-            "doc": "Event schema version"
+            "name": "entity_id",
+            "type": "string",
+            "doc": "ID of a the asset."
         },
         {
-            "name": "project_id",
+            "name": "entity_version",
             "type": "string",
-            "doc": "ID of a project"
+            "doc": "Aggregate revision"
+        },
+        {
+            "name": "domain_event_id",
+            "type": "string",
+            "doc": "ID of the last modifying event"
         },
         {
             "name": "name",
             "type": "string",
-            "doc": "Name of a project"
-        },
+            "doc": "name of the project"
+        }
     ]
 })
+
+
+def test_load_all_event_schemas():
+    with mock.patch('djangoevents.schema.list_concrete_aggregates') as la, \
+         mock.patch('djangoevents.schema.list_aggregate_events') as le:
+            la = [SampleEntity]
+
 
 
 def test_decode_cls_name():
@@ -39,20 +75,20 @@ def test_decode_cls_name():
     class Shortname:
         pass
 
-    assert decode_cls_name(LongName) == 'long_name'
-    assert decode_cls_name(Shortname) == 'shortname'
+    assert schema.decode_cls_name(LongName) == 'long_name'
+    assert schema.decode_cls_name(Shortname) == 'shortname'
 
 
 def test_parse_invalid_event_schema():
     fp = StringIO("Invalid schema")
 
     with pytest.raises(avro.schema.SchemaParseException):
-        parse_event_schema(fp)
+        schema.parse_event_schema(fp)
 
 
 def test_parse_valid_event_schema():
-    schema = parse_event_schema(SAMPLE_EVENT_SCHEMA)
-    assert schema is not None
+    evt_schema = schema.parse_event_schema(PROJECT_CREATED_SCHEMA)
+    assert evt_schema is not None
 
 
 def test_validate_invalid_event():
@@ -61,27 +97,26 @@ def test_validate_invalid_event():
             self.test = 'test'
 
     event = TestEvent()
-    schema = avro.schema.Parse(SAMPLE_EVENT_SCHEMA)
-    ret = validate_event(event, schema)
+    evt_schema = avro.schema.Parse(PROJECT_CREATED_SCHEMA)
+    ret = schema.validate_event(event, evt_schema)
     assert ret is False
 
 
 def test_validate_valid_event():
     class TestEvent:
         def __init__(self):
-            self.schema_version = 1
-            self.project_id = "XYZ"
+            self.entity_id = "1"
+            self.entity_version = "XYZ"
+            self.domain_event_id = "2"
             self.name = "Awesome Project"
 
     event = TestEvent()
-    schema = avro.schema.Parse(SAMPLE_EVENT_SCHEMA)
-    ret = validate_event(event, schema)
+    evt_schema = avro.schema.Parse(PROJECT_CREATED_SCHEMA)
+    ret = schema.validate_event(event, evt_schema)
     assert ret is True
 
 
 @override_settings(BASE_DIR='/path/to/proj/src/')
 def test_valid_event_to_schema_path():
-    from .test_domain import SampleEntity
-
-    avro_path = event_to_schema_path(aggregate_cls=SampleEntity, event_cls=SampleEntity.Created)
+    avro_path = schema.event_to_schema_path(aggregate_cls=SampleEntity, event_cls=SampleEntity.Created)
     assert avro_path == "/path/to/proj/avro/sample_entity/sample_entity_created_v1.json"
